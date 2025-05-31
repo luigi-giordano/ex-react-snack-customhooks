96a287d78d7b0906cb5fb4e6902dc0a2c3e75b77
luigi-giordano <luigi.giordano1992@gmail.com>
2025-05-31 12:58:56 +0200

    [milestone 2 completed]

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import useSwitch from "./useSwitch";
+// import useSwitch from "./useSwitch";
+
+// function App() {
+//   const [isOn, toggle] = useSwitch();
+
+//   return (
+//     <div>
+//       <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
+//       <button onClick={toggle}>Cambia Stato</button>
+//     </div>
+//   );
+// }
+
+import useDate from "./useDate";
 
 function App() {
-  const [isOn, toggle] = useSwitch();
+  const currentDate = useDate();
 
   return (
     <div>
-      <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
-      <button onClick={toggle}>Cambia Stato</button>
+      <h1>Data e ora attuali:</h1>
+      <p>{currentDate.toLocaleString()}</p>
     </div>
   );
 }
